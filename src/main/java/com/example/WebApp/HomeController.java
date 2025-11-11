ef5df7ad6869fde8a2837200ceae025cda4373c7
rebeka <rebekaandrade006@gmail.com>
2025-11-11 10:48:43 -0300

    1o commit

---
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author ALUNO
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody
+    String greeting() {
+        return "Hello, World!";
+    }
+
+    @RequestMapping("/Rebeka")
+    public @ResponseBody
+    String greetingFulano() {
+        return "Hello, Reeka!";
+    }
+
+}
