54f864acce61398b80954940aa93130843591460
“rebeka-andrade” <rebekaandrade006@gmail.com>
2025-12-03 10:44:29 -0300

    build quebrado

--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -56,7 +56,7 @@ class WebAppApplicationTests {
 
         @Test
         public void shouldReturnDefaultMessage4() throws Exception {
-                this.mockMvc.perform(get("/jenkins"))
+                this.mockMvc.perform(get("/Jenkins"))
                                 .andDo(print()).andExpect(status().isOk())
                                 .andExpect(content()
                                                 .string(containsString("jenkins funcionando!")));
